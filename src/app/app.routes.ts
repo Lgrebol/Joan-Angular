66f5efb99a9f4494c5813a10e0c548be57da7255
Lgrebol <lgrebol08@boscdelacoma.cat>
2024-10-14 17:07:12 +0200

    first commit

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { SignUpComponent } from './components/user/sign-up/sign-up.component';
-import { ListComponent } from './components/user/list/list.component';
-
-export const routes: Routes = [
-    {path: 'signup', component: SignUpComponent},
-    {path: 'listusers', component: ListComponent}
-];
